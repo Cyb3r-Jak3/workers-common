1a43f97fddcccd18b8192d4fdf60bf545b7adf3e
Cyb3r-Jak3 <git@cyberjake.xyz>
2026-06-12 17:57:38 -0400

    Upgrade other packages needed
    Signed-off-by: Cyb3r-Jak3 <git@cyberjake.xyz>

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
 // This is not actually used but needed for running the tests with cloudflare vitest
-import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+import { cloudflareTest } from "@cloudflare/vitest-pool-workers";
+import { defineConfig } from "vitest/config";
 
-export default defineWorkersConfig({
-  test: {
+export default defineConfig({
+    test: {
     coverage: {
         provider: 'istanbul',
         reporter: ['text', 'cobertura'],
     },
-    reporters: ['default', 'junit'],
-    outputFile: 'junit.xml',
-    poolOptions: {
-        workers: {
-            miniflare: {
-                compatibilityFlags: ["nodejs_compat"],
-                compatibilityDate: '2022-10-31',
-            }
-        }
-    }
-  },
+    },
+  plugins: [
+    cloudflareTest({
+      miniflare: {
+        compatibilityFlags: ["nodejs_compat"],
+        compatibilityDate: "2022-10-31",
+      },
+    }),
+  ],
 });
